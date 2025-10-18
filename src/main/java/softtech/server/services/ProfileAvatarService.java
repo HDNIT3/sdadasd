fe1b9df6fa23e0b1f7ee04248989e10daf36211d
HDNIT3 <nguyenhuynh.663459@gmail.com>
2025-10-18 13:32:51 +0700

    s

--- src/main/java/softtech/server/services/ProfileAvatarService.java
+++ src/main/java/softtech/server/services/ProfileAvatarService.java
@@ -2,6 +2,7 @@ package softtech.server.services;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import softtech.server.models.Avatar;
@@ -10,14 +11,23 @@ import softtech.server.repositories.AvatarRepo;
 import java.io.IOException;
 
 @Service
-@RequiredArgsConstructor
 public class ProfileAvatarService {
 
-    private final CloudinaryService cloudinaryService;
+    @Autowired(required = false)
+    private CloudinaryService cloudinaryService;
+    
     private final AvatarRepo avatarRepo;
+
+    public ProfileAvatarService(AvatarRepo avatarRepo) {
+        this.avatarRepo = avatarRepo;
+    }
 
     @Transactional
     public String uploadAvatar(String accountId, MultipartFile file) throws IOException {
+        if (cloudinaryService == null) {
+            throw new UnsupportedOperationException("Cloudinary service is not configured. Please set cloudinary.enabled=true and provide valid Cloudinary credentials.");
+        }
+        
         var result = cloudinaryService.uploadAvatar256(file, "avatars/" + accountId);
 
         Avatar avatar = avatarRepo.findByAccountId(accountId);
